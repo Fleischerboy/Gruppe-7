3296ff8137e62167b7ef19f1873dab1e563d9807
Thomas Gansmoe <thomasgansmoe@hotmail.com>
2022-11-04 12:48:19 +0100

    WTF!

--- Android/app/src/main/java/org/example/kotlin/android/app/ui/home/inbox/bid_system/bids/BidsOverviewFragment.kt
+++ Android/app/src/main/java/org/example/kotlin/android/app/ui/home/inbox/bid_system/bids/BidsOverviewFragment.kt
@@ -4,19 +4,57 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.example.kotlin.android.app.data.repository.BidRepository
 import org.example.kotlin.android.app.data.restapi.BidApi
+import org.example.kotlin.android.app.data.restapi.Resource
 import org.example.kotlin.android.app.databinding.FragmentBidsBinding
 import org.example.kotlin.android.app.ui.base.BaseFragment
+import org.example.kotlin.android.app.ui.handleApiError
 
 class BidsOverviewFragment : BaseFragment<BidsViewModel,FragmentBidsBinding, BidRepository>() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val userId = runBlocking {userPreferences.getUserId.first()}.toString()
+        viewModel.getAllProductsByUserId(userId)
+        val productsWithBidAdapter = ProductsWithBidListAdapter()
 
-        binding.textView3.text = "YAYY det funker nå!!"
+        binding.textViewBids.text = "WTF!"
+
+        binding.apply {
+            binding.ProductWithBidsRecycler.apply {
+                adapter = productsWithBidAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+
+        }
+
+        viewModel.ownersProducts.observe(viewLifecycleOwner) {
+            when(it) {
+                is Resource.Success -> {
+                    //binding.exploreProgressBar.visible(false);
+                    productsWithBidAdapter.submitList(it.value.toList())
+
+                }
+                is Resource.Loading -> {
+                    //binding.exploreProgressBar.visible(true);
+                }
+                is Resource.Failure -> {
+                    handleApiError(it)
+                }
+            }
+
+        }
+
     }
+
+
+
+
 
     override fun getViewModel(): Class<BidsViewModel> {
        return BidsViewModel::class.java
