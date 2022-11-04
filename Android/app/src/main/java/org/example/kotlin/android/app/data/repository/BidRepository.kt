c9d78b9d4b627592afafdde5d5d2021fba68e823
Thomas Gansmoe <thomasgansmoe@hotmail.com>
2022-11-04 11:59:26 +0100

    ready for fetching data

--- Android/app/src/main/java/org/example/kotlin/android/app/data/repository/BidRepository.kt
+++ Android/app/src/main/java/org/example/kotlin/android/app/data/repository/BidRepository.kt
@@ -2,13 +2,16 @@ package org.example.kotlin.android.app.data.repository
 
 import org.example.kotlin.android.app.data.restapi.BidApi
 
-class BidRepository(
-    private val api: BidApi
-) : BaseRepository() {
+class BidRepository(private val api: BidApi) : BaseRepository(){
 
-    suspend fun bids() = safeApiCall {
-
+    suspend fun getBidByBidId(bidId: String) {
+        safeApiCall {
+            api.getBidByBidId(bidId);
+        }
     }
 
-    
+    suspend fun getOwnersProductById(userId: String) {
+        safeApiCall { api.getUserProductsById(userId) }
+    }
+
 }
--- Android/app/src/main/java/org/example/kotlin/android/app/data/restapi/BidApi.kt
+++ Android/app/src/main/java/org/example/kotlin/android/app/data/restapi/BidApi.kt
@@ -1,6 +1,18 @@
 package org.example.kotlin.android.app.data.restapi
 
+import org.example.kotlin.android.app.data.responses.BidResponse
+import org.example.kotlin.android.app.data.responses.ProductResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface BidApi {
+
+    @GET("api/bids/{bidId}")
+    suspend fun getBidByBidId(@Path("bidId") bidId: String): BidResponse
+
+    //TODO Get bids from specific product
+    @GET("users/{userId}/products")
+    suspend fun getUserProductsById(@Path("userId") userId: String): List<ProductResponse>
 
 
 }
